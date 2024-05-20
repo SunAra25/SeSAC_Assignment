0416561af616308e4c7efcffe3df81b4872f0381
SunAra25 <tjsdkfk77@naver.com>
2024-05-20 19:53:43 +0900

    [Netflix] 검색 화면 AutoLayout 적용 및 중복 코드 개선

--- Netflix/Netflix/SearchViewController.swift
+++ Netflix/Netflix/SearchViewController.swift
@@ -15,6 +15,10 @@ class SearchViewController: UIViewController {
     @IBOutlet var resultLabel: UILabel!
     @IBOutlet var messageLabel: UILabel!
     
+    @IBOutlet var capsuleButton: [UIButton]!
+    let buttonImage = ["blue", "pink", "turquoise"]
+    let buttonTitle = ["공개예정", "모두의 인기 콘텐츠", "TOP 10 시리즈"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,32 +27,9 @@ class SearchViewController: UIViewController {
         searchTextField.placeholder = "게임, 시리즈, 영화를 검색하세요..."
         searchTextField.keyboardType = .default
         
-        releaseButton.setImage(UIImage.blue.withRenderingMode(.alwaysOriginal), for: .normal)
-        // 버튼의 스타일이 default일 때 이미지가 제대로 불러오지 못 함
-        // rendering mode를 original로 수정하면 됨
-        releaseButton.setTitle("공개예정", for: .normal)
-        
-        releaseButton.titleLabel?.font = .boldSystemFont(ofSize: 11) // Button Style이 Default일 때만 적용됨
-        releaseButton.tintColor = .systemBackground
-        releaseButton.backgroundColor = .label
-        releaseButton.layer.cornerRadius = 18
-        
-        /* iOS 15+
-        releaseButton.configuration?.baseForegroundColor = .systemBackground
-        releaseButton.configuration?.cornerStyle = .capsule
-        releaseButton.configuration?.baseBackgroundColor = .label
-         */
-        papularButton.setImage(UIImage.pink, for: .normal)
-        papularButton.setTitle("모두의 인기 콘텐츠", for: .normal)
-        papularButton.configuration?.baseForegroundColor = .label
-        papularButton.configuration?.cornerStyle = .capsule
-        papularButton.configuration?.baseBackgroundColor = .systemBackground
-        
-        top10Button.setImage(UIImage.turquoise, for: .normal)
-        top10Button.setTitle("TOP 10 시리즈", for: .normal)
-        top10Button.configuration?.baseForegroundColor = .label
-        top10Button.configuration?.cornerStyle = .capsule
-        top10Button.configuration?.baseBackgroundColor = .systemBackground
+        for (index, button) in capsuleButton.enumerated() {
+            setCapsuleBtnUI(button, image: UIImage(named: buttonImage[index]), title: buttonTitle[index])
+        }
         
         resultLabel.text = "검색어를 입력해보세요"
         resultLabel.textColor = .label
@@ -61,6 +42,18 @@ class SearchViewController: UIViewController {
         messageLabel.font = .systemFont(ofSize: 12, weight: .light)
     }
     
+    func setCapsuleBtnUI(_ button: UIButton, image: UIImage?, title: String) {
+        let backgroundColor: UIColor = button == releaseButton ? .label : .systemBackground
+        let tintColor: UIColor = button == releaseButton ? .systemBackground : .label
+        
+        button.setTitle(title, for: .normal)
+        button.setImage(image, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 11)
+        button.tintColor = tintColor
+        button.backgroundColor = backgroundColor
+        button.layer.cornerRadius = 18
+    }
+    
     @IBAction func returnDidTapped(_ sender: UITextField) {
         let result = searchTextField.text ?? ""
         resultLabel.text = "\(result)을/를 검색한 결과입니다."
@@ -70,50 +63,15 @@ class SearchViewController: UIViewController {
     @IBAction func categoryBtnDidTap(_ sender: UIButton) {
         let tag = sender.tag
         
-        switch tag {
-        case 0: 
-            setRelease()
-            messageLabel.text = "공개예정 결과입니다."
-        case 1: 
-            setPapular()
-            messageLabel.text = "인기검색 결과입니다."
-        case 2: 
-            setTop10()
-            messageLabel.text = "Top 10 결과입니다."
-        default: break
+        for i in 0..<capsuleButton.count {
+            if tag == i {
+                sender.backgroundColor = .label
+                sender.tintColor = .systemBackground
+                messageLabel.text = "\(buttonTitle[i]) 결과입니다."
+            } else {
+                capsuleButton[i].backgroundColor = .systemBackground
+                capsuleButton[i].tintColor = .label
+            }
         }
-    }
-    
-    func setRelease() {
-        releaseButton.configuration?.baseForegroundColor = .systemBackground
-        releaseButton.configuration?.baseBackgroundColor = .label
-        
-        papularButton.configuration?.baseForegroundColor = .label
-        papularButton.configuration?.baseBackgroundColor = .systemBackground
-        
-        top10Button.configuration?.baseForegroundColor = .label
-        top10Button.configuration?.baseBackgroundColor = .systemBackground
-    }
-    
-    func setPapular() {
-        releaseButton.configuration?.baseForegroundColor = .label
-        releaseButton.configuration?.baseBackgroundColor = .systemBackground
-        
-        papularButton.configuration?.baseForegroundColor = .systemBackground
-        papularButton.configuration?.baseBackgroundColor = .label
-        
-        top10Button.configuration?.baseForegroundColor = .label
-        top10Button.configuration?.baseBackgroundColor = .systemBackground
-    }
-    
-    func setTop10() {
-        releaseButton.configuration?.baseForegroundColor = .label
-        releaseButton.configuration?.baseBackgroundColor = .systemBackground
-        
-        papularButton.configuration?.baseForegroundColor = .label
-        papularButton.configuration?.baseBackgroundColor = .systemBackground
-        
-        top10Button.configuration?.baseForegroundColor = .systemBackground
-        top10Button.configuration?.baseBackgroundColor = .label
     }
 }

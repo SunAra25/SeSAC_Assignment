87d5caa00f6113264c5c138e5987440b6ef1058c
SunAra25 <tjsdkfk77@naver.com>
2024-05-20 16:25:51 +0900

    감정 다이어리 Outlet Collection 적용및 중복 코드 개선

--- EmotionWord/EmotionWord/EmotionViewController.swift
+++ EmotionWord/EmotionWord/EmotionViewController.swift
@@ -8,125 +8,36 @@
 import UIKit
 
 class EmotionViewController: UIViewController {
-    @IBOutlet var happyButton: UIButton!
-    @IBOutlet var happyLabel: UILabel!
-    @IBOutlet var loveButton: UIButton!
-    @IBOutlet var loveLabel: UILabel!
-    @IBOutlet var likeButton: UIButton!
-    @IBOutlet var likeLabel: UILabel!
-    @IBOutlet var upsetButton: UIButton!
-    @IBOutlet var upsetLabel: UILabel!
-    @IBOutlet var boringButton: UIButton!
-    @IBOutlet var boringLabel: UILabel!
-    @IBOutlet var relifeButton: UIButton!
-    @IBOutlet var relifeLabel: UILabel!
-    @IBOutlet var panicButton: UIButton!
-    @IBOutlet var panicLabel: UILabel!
-    @IBOutlet var disappointedButton: UIButton!
-    @IBOutlet var disappointedLabel: UILabel!
-    @IBOutlet var sadButton: UIButton!
-    @IBOutlet var sadLabel: UILabel!
+    @IBOutlet var emotionButtons: [UIButton]!
+    @IBOutlet var emotionLabels: [UILabel]!
     
-    var happyCount = 0
-    var loveCount = 0
-    var likeCount = 0
-    var upsetCount = 0
-    var boringCount = 0
-    var relifeCount = 0
-    var panicCount = 0
-    var disappointedCount = 0
-    var sadCount = 0
+    let emotions = ["행복해", "사랑해", "좋아해", "화가나", "지루해", "안심해", "당황해", "실망해", "속상해"]
+    var emotionCount = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        happyButton.setImage(UIImage.slime1, for: .normal)
-        happyButton.setTitle("", for: .normal)
-        
-        happyLabel.text = "행복해 0"
-        happyLabel.textAlignment = .center
-        
-        loveButton.setImage(UIImage.slime2, for: .normal)
-        loveButton.setTitle("", for: .normal)
-        
-        loveLabel.text = "사랑해 0"
-        loveLabel.textAlignment = .center
-
-        likeButton.setImage(UIImage.slime3, for: .normal)
-        likeButton.setTitle("", for: .normal)
-        
-        likeLabel.text = "좋아해 0"
-        likeLabel.textAlignment = .center
-        
-        upsetButton.setImage(UIImage.slime4, for: .normal)
-        upsetButton.setTitle("", for: .normal)
-        
-        upsetLabel.text = "화가나 0"
-        upsetLabel.textAlignment = .center
-        
-        boringButton.setImage(UIImage.slime5, for: .normal)
-        boringButton.setTitle("", for: .normal)
-        
-        boringLabel.text = "지루해 0"
-        boringLabel.textAlignment = .center
-        
-        relifeButton.setImage(UIImage.slime6, for: .normal)
-        relifeButton.setTitle("", for: .normal)
-        
-        relifeLabel.text = "안심해 0"
-        relifeLabel.textAlignment = .center
-        
-        panicButton.setImage(UIImage.slime7, for: .normal)
-        panicButton.setTitle("", for: .normal)
-        
-        panicLabel.text = "당황해 0"
-        panicLabel.textAlignment = .center
-        
-        disappointedButton.setImage(UIImage.slime8, for: .normal)
-        disappointedButton.setTitle("", for: .normal)
-        
-        disappointedLabel.text = "실망해 0"
-        disappointedLabel.textAlignment = .center
-        
-        sadButton.setImage(UIImage.slime9, for: .normal)
-        sadButton.setTitle("슬퍼해", for: .normal)
-        
-        sadLabel.text = "슬퍼해 0"
-        sadLabel.textAlignment = .center
+        for i in 0..<9 {
+            setButtonUI(emotionButtons[i], num: i)
+            setLabelUI(emotionLabels[i], title: emotions[i])
+        }
     }
 
+    func setLabelUI(_ label: UILabel, title: String) {
+        label.text = "\(title) 0"
+        label.textAlignment = .center
+    }
+    
+    func setButtonUI(_ button: UIButton, num: Int) {
+        let imageName = "slime\(num + 1)"
+        button.setImage(UIImage(named: imageName), for: .normal)
+        button.setTitle("", for: .normal)
+    }
+    
     @IBAction func slimeDidTapped(_ sender: UIButton) {
-        switch sender.tag {
-        case 0:
-            happyCount += 1
-            happyLabel.text = "행복해 \(happyCount)"
-        case 1:
-            loveCount += 1
-            loveLabel.text = "사랑해 \(loveCount)"
-        case 2:
-            likeCount += 1
-            likeLabel.text = "좋아해 \(likeCount)"
-        case 3:
-            upsetCount += 1
-            upsetLabel.text = "화가나 \(upsetCount)"
-        case 4:
-            boringCount += 1
-            boringLabel.text = "지루해 \(boringCount)"
-        case 5:
-            relifeCount += 1
-            relifeLabel.text = "안심해 \(relifeCount)"
-        case 6:
-            panicCount += 1
-            panicLabel.text = "당황해 \(panicCount)"
-        case 7:
-            disappointedCount += 1
-            disappointedLabel.text = "실망해 \(disappointedCount)"
-        case 8:
-            sadCount += 1
-            sadLabel.text = "슬퍼해 \(sadCount)"
-        default:
-            break
-        }
+        let index = sender.tag
+        emotionCount[index] += 1
+        emotionLabels[index].text = "\(emotions[index]) \(emotionCount[index])"
     }
 }
 

72c3316a1c2fa670ed53aebc67a23fb052da9767
SunAra25 <tjsdkfk77@naver.com>
2024-05-23 16:53:09 +0900

    [TableConstraints] 집중모드 뷰 구현

---
+++ TableConstaints/TableConstaints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableConstaints
+//
+//  Created by 아라 on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+

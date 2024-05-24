014c46c7361cd254370eb3f54e963cf74a993d89
SunAra25 <tjsdkfk77@naver.com>
2024-05-24 16:22:22 +0900

    [TableConstraints] 쇼핑 뷰 cell 연결

---
+++ TableConstaints/TableConstaints/ShoppingTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ShoppingTableViewCell.swift
+//  TableConstaints
+//
+//  Created by 아라 on 5/24/24.
+//
+
+import UIKit
+
+class ShoppingTableViewCell: UITableViewCell {
+    @IBOutlet var checkButton: UIButton!
+    @IBOutlet var starButton: UIButton!
+    @IBOutlet var titleLabel: UILabel!
+    
+    
+}
--- TableConstaints/TableConstaints/ShoppingTableViewController.swift
+++ TableConstaints/TableConstaints/ShoppingTableViewController.swift
@@ -13,16 +13,5 @@ class ShoppingTableViewController: UITableViewController {
         super.viewDidLoad()
 
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+    
 }

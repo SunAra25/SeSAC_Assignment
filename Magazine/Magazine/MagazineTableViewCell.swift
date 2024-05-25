485ad894eaa60c9ab815ef925b8455b5b4f1bffc
SunAra25 <tjsdkfk77@naver.com>
2024-05-25 20:38:14 +0900

    [Magazine] 여행 메거진 뷰 아울렛 연결 및 데이터 추가

---
+++ Magazine/Magazine/MagazineTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MagazineTableViewCell.swift
+//  Magazine
+//
+//  Created by 아라 on 5/25/24.
+//
+
+import UIKit
+
+class MagazineTableViewCell: UITableViewCell {
+    @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+}
---
+++ Magazine/Magazine/MagazineTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MagazineTableViewController.swift
+//  Magazine
+//
+//  Created by 아라 on 5/25/24.
+//
+
+import UIKit
+
+class MagazineTableViewController: UITableViewController {
+    @IBOutlet var headerLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}

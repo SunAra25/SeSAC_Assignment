a250321d376024a7b29c6b42f2863b7b9a03a316
SunAra25 <tjsdkfk77@naver.com>
2024-05-27 17:50:03 +0900

    [Travel] AdTableViewCell 세부 Layout 및 UI 적용

--- Magazine/Magazine/Travel/AdTableViewCell.swift
+++ Magazine/Magazine/Travel/AdTableViewCell.swift
@@ -8,16 +8,41 @@
 import UIKit
 
 class AdTableViewCell: UITableViewCell {
-
+    @IBOutlet var badgeLabel: UILabel!
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var colorView: UIView!
+    
+    let backgorundColors: [UIColor] = [
+        .systemRed.withAlphaComponent(0.1),
+        .systemGreen.withAlphaComponent(0.1),
+        .systemBlue.withAlphaComponent(0.1),
+        .systemPurple.withAlphaComponent(0.1)]
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        configureLayout()
     }
     
+    func configureLayout() {
+        colorView.layer.cornerRadius = 8
+        
+        badgeLabel.text = "AD"
+        badgeLabel.textColor = .black
+        badgeLabel.textAlignment = .center
+        badgeLabel.font = .systemFont(ofSize: 14)
+        badgeLabel.backgroundColor = .systemGray6
+        badgeLabel.clipsToBounds = true
+        badgeLabel.layer.cornerRadius = 8
+        
+        messageLabel.textColor = .black
+        messageLabel.textAlignment = .center
+        messageLabel.font = .systemFont(ofSize: 16, weight: .black)
+        messageLabel.numberOfLines = 0
+    }
+    
+    func configureCell(data: Travel) {
+        let index = Int.random(in: 0...100)
+        colorView.backgroundColor = backgorundColors[index % 4]
+        messageLabel.text = data.title
+    }
 }

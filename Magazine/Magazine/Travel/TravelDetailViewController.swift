e071ff1205b0353b0d8ff7bfcf12c1d3a2097882
SunAra25 <tjsdkfk77@naver.com>
2024-05-29 20:39:05 +0900

    [Travel] 관광지 디테일 뷰 네비게이션바 구현 및 뒤로가기 기능 구현

--- Magazine/Magazine/Travel/TravelDetailViewController.swift
+++ Magazine/Magazine/Travel/TravelDetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  TravelDetailViewController.swift
+//  TrableDetailViewController.swift
 //  Magazine
 //
 //  Created by 아라 on 5/29/24.
@@ -8,22 +8,19 @@
 import UIKit
 
 class TravelDetailViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let backbutton = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(popToPreviousView))
+        backbutton.tintColor = .black
+        
+        navigationItem.leftBarButtonItem = backbutton
 
-        // Do any additional setup after loading the view.
+        navigationItem.title = "관광지 화면"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func popToPreviousView() {
+        navigationController?.popViewController(animated: true)
     }
-    */
-
 }

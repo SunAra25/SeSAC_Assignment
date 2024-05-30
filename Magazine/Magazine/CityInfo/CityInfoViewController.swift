52be851922986af67b956f97c6ea3e52a8055691
SunAra25 <tjsdkfk77@naver.com>
2024-05-30 16:18:53 +0900

    [CityInfo] SegmentedControl로 tableView 필터링

--- Magazine/Magazine/CityInfo/CityInfoViewController.swift
+++ Magazine/Magazine/CityInfo/CityInfoViewController.swift
@@ -9,14 +9,22 @@ import UIKit
 
 class CityInfoViewController: UIViewController {
     @IBOutlet var cityTableView: UITableView!
+    @IBOutlet var segmentedControl: UISegmentedControl!
     
     let list = CityInfo.city
+    var filterList: [City] = []
+    
+    let items = ["전체", "국내", "해외"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "인기 도시"
+        
+        filterList = list
+        
         setTableView()
+        setSegment()
     }
     
     func setTableView() {
@@ -25,16 +33,54 @@ class CityInfoViewController: UIViewController {
         cityTableView.register(UINib(nibName: CityTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: CityTableViewCell.identifier)
         cityTableView.rowHeight = 150
     }
+    
+    func setSegment() {
+        segmentedControl.removeAllSegments()
+        
+        let actions = [showAllList, filterDomestic, filterOverseas]
+        
+        for idx in 0..<items.count {
+            segmentedControl.insertSegment(withTitle: items[idx], at: idx, animated: true)
+        }
+        
+        segmentedControl.addTarget(self, action: #selector(segmentBtnDidTap(segment:)), for: .valueChanged)
+        segmentedControl.selectedSegmentIndex = 0
+    }
+}
+
+extension CityInfoViewController {
+    func showAllList() {
+        filterList = list
+    }
+    
+    func filterDomestic() {
+        filterList = list.filter { $0.domestic_travel == true }
+        cityTableView.reloadData()
+    }
+    
+    func filterOverseas() {
+        filterList = list.filter { $0.domestic_travel == false }
+        cityTableView.reloadData()
+    }
+    
+    @objc func segmentBtnDidTap(segment: UISegmentedControl) {
+        switch segment.selectedSegmentIndex {
+        case 0: showAllList()
+        case 1: filterDomestic()
+        case 2: filterOverseas()
+        default: break
+        }
+    }
 }
 
 extension CityInfoViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        list.count
+        filterList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = cityTableView.dequeueReusableCell(withIdentifier: CityTableViewCell.identifier, for: indexPath) as! CityTableViewCell
-        let data = list[indexPath.row]
+        let data = filterList[indexPath.row]
         
         cell.configureCell(data)
         

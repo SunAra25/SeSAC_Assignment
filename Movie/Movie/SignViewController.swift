f2c03c16aed689b916ac611e8a851f6b649db349
SunAra25 <tjsdkfk77@naver.com>
2024-06-05 00:05:19 +0900

    [Movie] 컴포넌트 초기값 설정

--- Movie/Movie/SignViewController.swift
+++ Movie/Movie/SignViewController.swift
@@ -6,24 +6,105 @@
 //
 
 import UIKit
+import SnapKit
 
 class SignViewController: UIViewController {
-
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "SUNFLIX"
+        label.textAlignment = .center
+        label.textColor = .red
+        label.font = .systemFont(ofSize: 24, weight: .heavy)
+        return label
+    }()
+    let idTextField: UITextField = {
+        let textField = UITextField()
+        textField.text = "이메일 주소 또는 전화번호"
+        textField.textColor = .white
+        textField.textAlignment = .center
+        textField.font = .systemFont(ofSize: 16)
+        textField.backgroundColor = .gray
+        textField.layer.cornerRadius = 12
+        return textField
+    }()
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.text = "비밀번호"
+        textField.textColor = .white
+        textField.textAlignment = .center
+        textField.font = .systemFont(ofSize: 16)
+        textField.backgroundColor = .gray
+        textField.layer.cornerRadius = 12
+        return textField
+    }()
+    let nicknameTextField: UITextField = {
+        let textField = UITextField()
+        textField.text = "닉네임"
+        textField.textColor = .white
+        textField.textAlignment = .center
+        textField.font = .systemFont(ofSize: 16)
+        textField.backgroundColor = .gray
+        textField.layer.cornerRadius = 12
+        return textField
+    }()
+    let locationTextField: UITextField = {
+        let textField = UITextField()
+        textField.text = "위치"
+        textField.textColor = .white
+        textField.textAlignment = .center
+        textField.font = .systemFont(ofSize: 16)
+        textField.backgroundColor = .gray
+        textField.layer.cornerRadius = 12
+        return textField
+    }()
+    let codeTextField: UITextField = {
+        let textField = UITextField()
+        textField.text = "추천 코드 입력"
+        textField.textColor = .white
+        textField.textAlignment = .center
+        textField.font = .systemFont(ofSize: 16)
+        textField.backgroundColor = .gray
+        textField.layer.cornerRadius = 12
+        return textField
+    }()
+    let signUpButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("회원가입", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 12
+        return button
+    }()
+    let infoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "추가 정보 입력"
+        label.textAlignment = .left
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 17)
+        return label
+    }()
+    let switchView: UISwitch = {
+        let view = UISwitch()
+        view.isOn = true
+        view.onTintColor = .red
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureView()
+        setHierachy()
+        setConstraints()
+    }
 
-        // Do any additional setup after loading the view.
+    func configureView() {
+        view.backgroundColor = .black
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setHierachy() {
     }
-    */
 
+    func setConstraints() {
+    }
 }
+

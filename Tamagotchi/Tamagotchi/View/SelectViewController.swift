ed1ff77f513ca1c832060ce2b397a79219d9ec3f
SunAra25 <tjsdkfk77@naver.com>
2024-06-09 13:17:54 +0900

    [Tamagotchi] 선택 뷰 컬렉션 뷰 세부 설정 및 테스트

--- Tamagotchi/Tamagotchi/View/SelectViewController.swift
+++ Tamagotchi/Tamagotchi/View/SelectViewController.swift
@@ -10,7 +10,7 @@ import SnapKit
 
 class SelectViewController: BaseViewController {
     let titleLabel = UILabel()
-    let tamagoCollectionView = UICollectionView()
+    let tamagoCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +27,11 @@ class SelectViewController: BaseViewController {
         titleLabel.textColor = .black
         titleLabel.textAlignment = .center
         titleLabel.font = .bodyB
+        
+        tamagoCollectionView.delegate = self
+        tamagoCollectionView.dataSource = self
+        tamagoCollectionView.register(TamagotchiCollectionViewCell.self, forCellWithReuseIdentifier: TamagotchiCollectionViewCell.identifier)
+        tamagoCollectionView.backgroundColor = .clear
     }
     
     func setHierarchy() {
@@ -41,7 +46,43 @@ class SelectViewController: BaseViewController {
         }
         
         tamagoCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(20)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
         }
+    }
+}
+
+extension SelectViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TamagotchiCollectionViewCell.identifier, for: indexPath) as! TamagotchiCollectionViewCell
+        
+        cell.imageView.image = UIImage.no
+        cell.nameLabel.text = "반짝반짝 다마고치"
+        return cell
+    }
+}
+
+extension SelectViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let inset: CGFloat = 8
+        let padding: CGFloat = 8
+        let collectionViewSize = screenWidth - inset * 2 - padding * 4
+        
+        let cellSize = collectionViewSize / 3
+        return CGSize(width: cellSize, height: cellSize)
     }
 }
---
+++ Tamagotchi/Tamagotchi/View/TamagotchiCollectionViewCell.swift
@@ -0,0 +1,59 @@
+//
+//  TamagotchiCollectionViewCell.swift
+//  Tamagotchi
+//
+//  Created by 아라 on 6/8/24.
+//
+
+import UIKit
+import SnapKit
+
+class TamagotchiCollectionViewCell: UICollectionViewCell {
+    static let identifier = "TamagotchiCollectionViewCell"
+    let imageView = UIImageView()
+    let nameView = UIView()
+    let nameLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureView() {
+        nameView.layer.cornerRadius = 5
+        nameView.layer.borderWidth = 1
+        nameView.layer.borderColor = UIColor.border?.cgColor
+        
+        nameLabel.textColor = .font
+        nameLabel.textAlignment = .center
+        nameLabel.font = .capB
+    }
+    
+    func setLayout() {
+        contentView.addSubview(imageView)
+        contentView.addSubview(nameView)
+        nameView.addSubview(nameLabel)
+        
+        imageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(4)
+            make.horizontalEdges.equalToSuperview().inset(16)
+            make.width.equalTo(imageView.snp.height)
+        }
+        
+        nameView.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(4)
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().inset(4)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.verticalEdges.equalToSuperview().inset(2)
+            make.horizontalEdges.equalToSuperview().inset(4)
+        }
+    }
+}

d18b617b502f560001b4652f9164774170baef67
SunAra25 <tjsdkfk77@naver.com>
2024-06-12 00:31:58 +0900

    [MovieSearch] 프로젝트 생성 및 초기 세팅

---
+++ MovieSearch/MovieSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by 아라 on 6/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

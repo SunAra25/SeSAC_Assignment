d35b52b5a579c1309858cd3a06328e5f23871d9e
SunAra25 <tjsdkfk77@naver.com>
2024-06-19 19:57:25 +0900

    [ResturantMap] 사용자 위치 권한 요청

--- Magazine/Magazine/Restaurant/RestaurantMapViewController.swift
+++ Magazine/Magazine/Restaurant/RestaurantMapViewController.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
+import CoreLocation
 import MapKit
 
 class RestaurantMapViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet var mapView: MKMapView!
     @IBOutlet var segmentedCtr: UISegmentedControl!
-    
+    let locationManager = CLLocationManager()
     let list = RestaurantList().restaurantArray
     var filterList: [Restaurant] = []
     lazy var categoryList = ["전체"] + Array(Set<String>(self.list.map { $0.category })).sorted { $0 > $1}
@@ -22,15 +23,15 @@ class RestaurantMapViewController: UIViewController, MKMapViewDelegate {
         filterList = list
         
         mapView.delegate = self
-        
-        let center = CLLocationCoordinate2D(latitude: 37.517922, longitude: 126.886468)
-        mapView.region = MKCoordinateRegion(center: center, latitudinalMeters: 500, longitudinalMeters: 500)
+        locationManager.delegate = self
         
         let annotations = setRestaurantAnnotation()
         
         mapView.addAnnotations(annotations)
         
         setSegmentedControl()
+        
+        checkDeviceLocationAuthorization()
     }
     
     func setSegmentedControl() {
@@ -74,4 +75,56 @@ class RestaurantMapViewController: UIViewController, MKMapViewDelegate {
         
         mapView.addAnnotations(newAnno)
     }
+}
+
+extension RestaurantMapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(#function)
+        if let coordinate = locations.last?.coordinate {
+            setRegionAndAnnotation(center: coordinate)
+        }
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print(#function)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print(#function, "iOS14+")
+        checkCurrentLocationAuthorization()
+    }
+}
+
+extension RestaurantMapViewController {
+    func setRegionAndAnnotation(center: CLLocationCoordinate2D) {
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: 500, longitudinalMeters: 500)
+        
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func checkDeviceLocationAuthorization() {
+        if CLLocationManager.locationServicesEnabled() {
+            checkCurrentLocationAuthorization()
+        } else {
+            // TODO: 설정 유도
+        }
+    }
+    
+    func checkCurrentLocationAuthorization() {
+        var status = locationManager.authorizationStatus
+        
+        switch status {
+        case .notDetermined:
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+        case .denied:
+            // TODO: 설정 유도
+            break
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        default: print(status)
+        }
+    }
+
 }

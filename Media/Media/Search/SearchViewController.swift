27714eeb2e2489c1075c51bb2453b710f4889995
SunAra25 <tjsdkfk77@naver.com>
2024-06-27 17:02:55 +0900

    [Media] 영화 검색 뷰 구현

---
+++ Media/Media/Search/SearchViewController.swift
@@ -0,0 +1,62 @@
+//
+//  SearchViewController.swift
+//  Media
+//
+//  Created by 아라 on 6/27/24.
+//
+
+import UIKit
+import SnapKit
+
+class SearchViewController: UIViewController {
+    let searchBar = UISearchBar()
+    let tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        searchBar.delegate = self
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.identifier)
+        tableView.rowHeight = 60
+        
+        setLayout()
+    }
+    
+    func setLayout() {
+        view.addSubview(searchBar)
+        view.addSubview(tableView)
+        
+        searchBar.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(40)
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // TODO: 네트워크
+    }
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchTableViewCell.identifier, for: indexPath) as? SearchTableViewCell else { return UITableViewCell() }
+        
+        return cell
+    }
+}

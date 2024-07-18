f03af5894fb0b4208d88c91310e8e1db480268ff
SunAra25 <tjsdkfk77@naver.com>
2024-07-18 18:37:24 +0900

    [Diffable] 프로젝트 생성 및 초기 세팅

---
+++ DiffablePractice/DiffablePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiffablePractice
+//
+//  Created by 아라 on 7/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
